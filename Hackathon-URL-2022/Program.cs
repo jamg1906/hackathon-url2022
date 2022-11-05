31ccbb37741dc2364f501c271ab5c7e0ee025185
Javier Andrés <54732923+jamg1906@users.noreply.github.com>
2022-11-05 14:23:08 -0600

    Initial commit

---
+++ Hackathon-URL-2022/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hackathon_URL_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
